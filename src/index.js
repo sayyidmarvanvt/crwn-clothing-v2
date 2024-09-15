3bbececa1e67ff1a79fe0760f3f47486b07eeb3c
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-09-15 20:56:12 +0530

    update to latest router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
