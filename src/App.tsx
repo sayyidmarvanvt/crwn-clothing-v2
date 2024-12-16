9441a6bf4e537bef904b0990494e14300a8fe098
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2024-12-16 20:31:40 +0300

    code splitting done using lazy and Suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,34 @@
-import React, { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import SignUpForm from "./routes/sign-up-form/sign-up-form.component";
-import SignInForm from "./routes/sign-in-form/sign-in-form.component";
-import Shop from "./routes/shop/shop.component";
-import Checkout from "./routes/checkout/checkout.component";
-import NotFound from "./routes/not-found/not-found.component";
+
+import Spinner from "./components/spinner/spinner.component";
 import { checkUserSession } from "./store/user/user.action";
+
+// Lazy-loaded components
+const Navigation = lazy(
+  () => import("./routes/navigation/navigation.component")
+);
+const Home = lazy(() => import("./routes/home/home.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const SignUpForm = lazy(
+  () => import("./routes/sign-up-form/sign-up-form.component")
+);
+const SignInForm = lazy(
+  () => import("./routes/sign-in-form/sign-in-form.component")
+);
+const NotFound = lazy(() => import("./routes/not-found/not-found.component"));
 
 const App = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(checkUserSession());
   }, [dispatch]);
+
   return (
-    <>
+    <Suspense fallback={<Spinner />}>
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<Home />} />
@@ -27,7 +39,7 @@ const App = () => {
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </>
+    </Suspense>
   );
 };
 
